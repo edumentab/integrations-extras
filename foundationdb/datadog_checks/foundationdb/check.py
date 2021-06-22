a078d51e31f74fda4b089f0f4545a5fe8c5bcc94
vendethiel <vendethiel@hotmail.fr>
2021-06-22 18:22:25 +0200

    Use monotonic_count instead of count

--- foundationdb/datadog_checks/foundationdb/check.py
+++ foundationdb/datadog_checks/foundationdb/check.py
@@ -20,19 +20,6 @@ class FoundationdbCheck(AgentCheck):
         fdb_args.append('status json')
         return get_subprocess_output(fdb_args, self.log)
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
-
     def check(self, _):
         try:
             status = self.fdb_status_data()
@@ -144,8 +131,6 @@ class FoundationdbCheck(AgentCheck):
             raise ValueError("JSON Status data doesn't include cluster data")
 
         cluster = status["cluster"]
-        if "degraded_processes" in cluster:
-            self.gauge("foundationdb.degraded_processes", cluster["degraded_processes"])
         if "machines" in cluster:
             self.gauge("foundationdb.machines", len(cluster["machines"]))
         if "processes" in cluster:
@@ -186,7 +171,15 @@ class FoundationdbCheck(AgentCheck):
             for k, v in cluster["latency_probe"].items():
                 self.gauge("foundationdb.latency_probe." + k, v)
 
-        self.service_check("foundationdb.can_connect", AgentCheck.OK)
+        degraded_processes = 0
+        if "degraded_processes" in cluster:
+            self.gauge("foundationdb.degraded_processes", cluster["degraded_processes"])
+            degraded_processes = cluster["degraded_processes"]
+
+        if degraded_processes > 0:
+            self.service_check("foundationdb.can_connect", AgentCheck.WARNING, message="There are degraded processes")
+        else:
+            self.service_check("foundationdb.can_connect", AgentCheck.OK)
 
     def maybe_gauge(self, metric, obj, key, tags=None):
         if key in obj:
@@ -194,73 +187,11 @@ class FoundationdbCheck(AgentCheck):
 
     def maybe_count(self, metric, obj, key, tags=None):
         if key in obj:
-            self.count(metric, obj[key], tags=tags)
+            self.monotonic_count(metric, obj[key], tags=tags)
 
     def maybe_hz_counter(self, metric, obj, key, tags=None):
         if key in obj:
             if "hz" in obj[key]:
                 self.gauge(metric + ".hz", obj[key]["hz"], tags=tags)
             if "counter" in obj[key]:
-                self.count(metric + ".counter", obj[key]["counter"], tags=tags)
-
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
-
-        # Use self.instance to read the check configuration
-        # url = self.instance.get("url")
-
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
-
-        # except Timeout as e:
-        #     self.service_check(
-        #         "foundationdb.can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(url, e),
-        #     )
-        #     raise
-
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "foundationdb.can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(url, e),
-        #     )
-        #     raise
-
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "foundationdb.can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(url, e),
-        #     )
-        #     raise
-
-        # except ValueError as e:
-        #     self.service_check(
-        #         "foundationdb.can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-
-        # If your check ran successfully, you can send the status.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("foundationdb.can_connect", AgentCheck.OK)
+                self.monotonic_count(metric + ".counter", obj[key]["counter"], tags=tags)
